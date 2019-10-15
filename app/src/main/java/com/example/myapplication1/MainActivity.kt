28512180db3a13fe86955fdf555ac89e8c852607
Alejandro <Canarygo1@gmail.com>
2019-10-15 21:54:44 +0200

    change on main Activity

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -9,31 +9,28 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-<<<<<<< HEAD
+
     private val name:String = "Alejandro Cruz"
     private val city:String = "S/C de Tenerife"
     private val description:String = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 
     override fun onCreate(savedInstanceState: Bundle?) {
-=======
-  override fun onCreate(savedInstanceState: Bundle?) {
->>>>>>> production
+
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main)
 
     val button:Button = findViewById(R.id.myButton)
     button.setOnClickListener {
         val intent = Intent(this, SecondActivity::class.java).apply {
-<<<<<<< HEAD
+
             putExtra("name",  name) 
             putExtra("city", city)
             putExtra("description",description)
 
         }
-=======
+
 
     }
->>>>>>> production
         startActivity(intent)
   }
  }
