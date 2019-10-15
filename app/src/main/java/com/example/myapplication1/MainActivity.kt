b9d18bd16fdd7128fc7a34b5d420b18c18d0cf54
Alejandro <Canarygo1@gmail.com>
2019-10-15 22:19:23 +0200

    finish SecondAcitivity code ready for review!

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -13,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     private val name:String = "Alejandro Cruz"
     private val city:String = "S/C de Tenerife"
     private val description:String = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-
+    private val birthday:String = "02/06/1999"
     override fun onCreate(savedInstanceState: Bundle?) {
 
     super.onCreate(savedInstanceState);
@@ -22,15 +23,12 @@ class MainActivity : AppCompatActivity() {
     val button:Button = findViewById(R.id.myButton)
     button.setOnClickListener {
         val intent = Intent(this, SecondActivity::class.java).apply {
-
             putExtra("name",  name) 
             putExtra("city", city)
             putExtra("description",description)
-
+            putExtra("birthday", birthday)
         }
-
-
-    }
         startActivity(intent)
+    }
   }
  }
