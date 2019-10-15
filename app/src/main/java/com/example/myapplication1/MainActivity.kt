2216b07f71d369f74ab42ca958ebcf5057933446
Alejandro <Canarygo1@gmail.com>
2019-10-15 13:25:49 +0200

    xml LinearLayout

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.example.myapplication1
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-var count = 0;
+  var count = 0;
+  private lateinit var profileButton: Button
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main)
-
+    profileButton = findViewById(R.id.profileButton)
     val boton:Button = findViewById(R.id.myButton)
     boton.setOnClickListener {
       Toast.makeText(MainActivity@this, "${count++}", Toast.LENGTH_SHORT).show()
       val text: TextView = findViewById(R.id.texto)
+
       text.text = count.toString()
-      
       boton.text = "Click again!"
+    }
+    profileButton.setOnClickListener{
+      val intent = Intent(this, ProfileActivity::class.java)
     }
   }
 }
